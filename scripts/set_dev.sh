86ffa58604a66dad24fb548c53f14c527cee73c6
vitali2y <vitaliyy@gmail.com>
2018-05-16 23:00:47 +0300

    Some fixes related to 'npm run dev'

--- scripts/set_dev.sh
+++ scripts/set_dev.sh
@@ -9,6 +9,6 @@ rm -f ./util/netconf.coffee
 cp ./scripts/netconf_dev.coffee.tmpl ./util/netconf.coffee
 
 # configuring clients (with local rendezvous server)
-(cd ./cfg; for f in ./*; do cp $f/* ../dist/linux/$f; done; cd -)
-(cd ./cfg; for f in ./*; do cp $f/* ../dist/darwin/$f; done; cd -)
-(cd ./cfg; for f in ./*; do cp $f/* ../dist/windows/$f; done; cd -)
+(cd ./cfg; for f in ./*; do mkdir -p ../dist/linux/$f; cp $f/* ../dist/linux/$f; done; cd -)
+(cd ./cfg; for f in ./*; do mkdir -p ../dist/darwin/$f; cp $f/* ../dist/darwin/$f; done; cd -)
+(cd ./cfg; for f in ./*; do mkdir -p ../dist/windows/$f; cp $f/* ../dist/windows/$f; done; cd -)
